c34fa5f8d99875fcbfd5730c759dcccb4395eb35
RLKhoury <rakanlkhoury@gmail.com>
2020-11-03 14:04:15 -0500

    Generated receiver positions and sample outputs + Receiver positions file with selected areas (corridors)

---
+++ Simulator Interface/ReceiverPositions/GenerateEnvironmentReceiverPositions.bat
@@ -0,0 +1,45 @@
+
+REM Lower Corridor
+set bottomLeft=(-13,-28,1.5)
+set topRight=(-8.5,-8,1.5)
+set numX=10
+set numY=41
+set numZ=1
+set outputFile="SampleReceiverPositions/ReceiverPositions_lowerCorridor.dat"
+python GenerateReceiverPositions.py %bottomLeft% %topRight% --numX %numX% --numY %numY% --numZ %numZ% --filePath %outputFile%
+
+REM Main Room
+set bottomLeft=(-29.5,-7.5,1.5)
+set topRight=(-8.5,10.5,1.5)
+set numX=43
+set numY=37
+set numZ=1
+set outputFile="SampleReceiverPositions/ReceiverPositions_mainRoom.dat"
+python GenerateReceiverPositions.py %bottomLeft% %topRight% --numX %numX% --numY %numY% --numZ %numZ% --filePath %outputFile%
+
+REM Connecting Corridor
+set bottomLeft=(-12.5,11,1.5)
+set topRight=(-8.5,13,1.5)
+set numX=9
+set numY=5
+set numZ=1
+set outputFile="SampleReceiverPositions/ReceiverPositions_connectingCorridor.dat"
+python GenerateReceiverPositions.py %bottomLeft% %topRight% --numX %numX% --numY %numY% --numZ %numZ% --filePath %outputFile%
+
+REM Main Corridor
+set bottomLeft=(-43.5,13.5,1.5)
+set topRight=(33,20.5,1.5)
+set numX=154
+set numY=15
+set numZ=1
+set outputFile="SampleReceiverPositions/ReceiverPositions_mainCorridor.dat"
+python GenerateReceiverPositions.py %bottomLeft% %topRight% --numX %numX% --numY %numY% --numZ %numZ% --filePath %outputFile%
+
+REM Upper Corridor
+set bottomLeft=(21,21,1.5)
+set topRight=(24,31.5,1.5)
+set numX=7
+set numY=22
+set numZ=1
+set outputFile="SampleReceiverPositions/ReceiverPositions_upperCorridor.dat"
+python GenerateReceiverPositions.py %bottomLeft% %topRight% --numX %numX% --numY %numY% --numZ %numZ% --filePath %outputFile%
